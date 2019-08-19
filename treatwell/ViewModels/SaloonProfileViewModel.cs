4f2bcdd99e5390416ad6d68dd381f428c4e3280a
Zubair Hasan <zubairhasan353@gmail.com>
2019-08-19 15:03:14 +0500

    update SaloonProfileApiController

--- treatwell/ViewModels/SaloonProfileViewModel.cs
+++ treatwell/ViewModels/SaloonProfileViewModel.cs
@@ -14,15 +14,13 @@ namespace treatwell.ViewModels
         public List<VenueServicesDto> venueServices { get; set; }
         public List<CustomerReviewsDto> CustomerReviews { get; set; }
         public List<BookingDaysTimeDto> BookingDaysTimes { get; set; }
-        //public List<UserVenuesDto> Employees { get; set; }
-
+        
         public SaloonProfileViewModel()
         {
             VenueImages = new List<VenueImagesDto>();
             CustomerReviews = new List<CustomerReviewsDto>();
             venueServices = new List<VenueServicesDto>();
             BookingDaysTimes = new List<BookingDaysTimeDto>();
-            //Employees = new List<UserVenuesDto>();
         }
     }
 }
